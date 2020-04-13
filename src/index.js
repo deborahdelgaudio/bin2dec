0a16bdaddc764ea1f448196fa6ccb59e2cc4c7c4
Deborah Del Gaudio <delgaudio.d@gmail.com>
2020-04-13 15:05:06 +0200

    add first draft of bin2dec function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('Hello :)');
+// Prendo gli argomenti della riga di comando DONE
+// Valido gli argomenti (le uniche cifre ammesse sono 0 ed 1) DONE
+// Conversione da binario a decimale DONE 
+// Tornare il risultato e stamparlo a video
+
+function bin2dec(string) {
+  if (string.match(/^[0-1]+$/g) === null) {
+    throw new Error('A binary number is made of only 0 and 1');
+  }
+
+  const numbers = string.split('').reverse();
+  let sum = 0;
+
+  numbers.forEach((value, index) => {
+    sum += (value * Math.pow(2, index))
+  });
+  
+  return sum;
+}
+
+var number = process.argv.slice(2)[0];
+console.log(bin2dec(number));
