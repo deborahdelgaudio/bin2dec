8c9c120178090105249fcb879f5a7c2a99621819
Deborah Del Gaudio <delgaudio.d@gmail.com>
2020-04-13 16:47:21 +0200

    fix export in package.json
    Co-authored-by: Gabriele D'Arrigo <darrigo.g@gmail.com>

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,7 @@
+#!/usr/bin/env node
 
 const chalk = require('chalk');
-const bin2dec = require('./bin2dec');
+const bin2dec = require('../src/bin2dec');
 
 function log(binary, decimal) {
   console.log(`${chalk.magenta(binary)} -> ${chalk.greenBright(decimal)}`);
