491043a934dcadaff633739d200c3979a6bd1a30
Deborah Del Gaudio <delgaudio.d@gmail.com>
2020-04-13 16:02:14 +0200

    add chalk to format output

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
+
+const chalk = require('chalk');
 const bin2dec = require('./bin2dec');
+
+function log(binary, decimal) {
+  console.log(`${chalk.magenta(binary)} -> ${chalk.greenBright(decimal)}`);
+}
 
 function cli(args) {
   args.forEach((binary) => {
     const decimal = bin2dec(binary);
-    console.log(`${binary} -> ${decimal}`);
+    log(binary, decimal);
   });
 }
 
